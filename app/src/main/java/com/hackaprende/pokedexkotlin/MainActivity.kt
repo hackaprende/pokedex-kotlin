bb544ca52c6cc00cf083086b1c989e5425fefd57
Jesus Almaral <almaral17@gmail.com>
2020-09-06 15:24:38 -0500

    Pass data to DetailFragment

--- app/src/main/java/com/hackaprende/pokedexkotlin/MainActivity.kt
+++ app/src/main/java/com/hackaprende/pokedexkotlin/MainActivity.kt
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity(), ListFragment.PokemonSelectListener {
+    private lateinit var detailFragment: DetailFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        detailFragment = supportFragmentManager.findFragmentById(R.id.detail_fragment) as DetailFragment
     }
 
     override fun onPokemonSelected(pokemon: Pokemon) {
-        TODO("Not yet implemented")
+        detailFragment.setPokemonData(pokemon)
     }
 }
